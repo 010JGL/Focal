9527884f2a72d6440f5b67a8c4f99b2d410c4aaf
Jeremy <010JGL@GMAIL.COM>
2023-03-08 12:47:26 -0500

    test commit

---
+++ pig_latin_translator.js
@@ -0,0 +1,20 @@
+var originalWords = process.argv.slice(2);
+var pigLatinWords = [];
+
+for (var i = 0; i < originalWords.length; i++) {
+    // console.log(originalWords[i]);
+  // console.log(translateToPigLatin(originalWords[i]));
+    // pour debug
+  pigLatinWords.push(translateToPigLatin(originalWords[i]));
+}
+
+console.log(pigLatinWords.join(' '));
+
+function translateToPigLatin(word) {
+//  console.log("Word", word);        pour debugger
+// console.log("First letter", word[0]);
+//  console.log("Rest of word", word.slice(2, word.length)); 
+  
+  
+  return word.slice(1, word.length) + word[0] + "ay";
+}
