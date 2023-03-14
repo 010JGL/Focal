0751cfd1422d0e5ae509efb2e35d23c0462ac772
Jeremy <010JGL@GMAIL.COM>
2023-03-13 23:20:18 -0400

    Comments and eslint

--- reverse.js
+++ reverse.js
@@ -3,6 +3,6 @@ const input = process.argv[2];     // put an input with the node cmd
 if (input) {                        // if there is a num cmd, console.log the func
   console.log(reverse(input));
 }
-function reverse(original) {        // function to reverse the letters
-  return original.split('').reverse().join('');
-}
+const reverse = function(original) {        // function to reverse the letters
+  return original.split('').reverse().join('');  // split the data, reverse it and join it back together
+};
